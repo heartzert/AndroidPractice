27b346796dbf1c4428f64d84777b038efb80b3e3
wangxinze <wangxinze@oxyzgroup.com>
2019-03-26 14:37:44 +0800

    init

---
+++ app/src/main/java/heartzert/test/all/MainActivity.kt
@@ -0,0 +1,19 @@
+package heartzert.test.all
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import heartzert.test.all.nestedscrollview.NestedScrollViewAct
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun nestedScrollView(view: View) {
+        startActivity(Intent(this, NestedScrollViewAct::class.java))
+    }
+}
---
+++ app/src/main/java/heartzert/test/all/nestedscrollview/NestedScrollViewAct.kt
@@ -0,0 +1,13 @@
+package heartzert.test.all.nestedscrollview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import heartzert.test.all.R
+
+class NestedScrollViewAct : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nested_scroll_view)
+    }
+}
