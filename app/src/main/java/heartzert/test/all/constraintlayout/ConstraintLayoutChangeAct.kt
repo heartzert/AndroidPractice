b58680dfdc0b7c231588e569f9c01cce1f4dd9b0
wangxinze <wangxinze@oxyzgroup.com>
2019-03-28 21:01:11 +0800

    test constraintSet

--- app/src/main/java/heartzert/test/all/constraintlayout/ConstraintLayoutChangeAct.kt
+++ app/src/main/java/heartzert/test/all/constraintlayout/ConstraintLayoutChangeAct.kt
@@ -15,6 +15,10 @@ import kotlinx.android.synthetic.main.activity_constraint_layout_change.full_lay
 class ConstraintLayoutChangeAct : AppCompatActivity() {
     private var storeConstraintSet: ConstraintSet? = null
 
+    companion object {
+        var times = 0
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_constraint_layout_change)
@@ -26,7 +30,20 @@ class ConstraintLayoutChangeAct : AppCompatActivity() {
         }
         Log.d("======", "dianle test")
         TransitionManager.beginDelayedTransition(full_layout)
-        test2()
+        when (times) {
+            0 -> {
+                test1();times++
+            }
+            1 -> {
+                test2();times++
+            }
+            2 -> {
+                test3();times++
+            }
+            else -> {
+                reset(view);times = 0
+            }
+        }
     }
 
     fun reset(view: View) {
@@ -44,7 +61,25 @@ class ConstraintLayoutChangeAct : AppCompatActivity() {
     }
 
     private fun test2() {
+        ConstraintSet()
+            .apply { clone(full_layout) }
+            .apply { clearConstraint(this, button1, true) }
+            .apply { clearConstraint(this, button2, true) }
+            .apply { clearConstraint(this, button3, true) }
+            .apply {
+                //这里的参数不能为Constraint.Start/ConstraintSet.End否则会报错
+                createHorizontalChain(
+                    ConstraintSet.PARENT_ID, ConstraintSet.LEFT,
+                    ConstraintSet.PARENT_ID, ConstraintSet.RIGHT,
+                    intArrayOf(button1.id, button2.id, button3.id),
+                    null,
+                    ConstraintSet.CHAIN_PACKED
+                )
+            }
+            .applyTo(full_layout)
+    }
 
+    private fun test3() {
         ConstraintSet()
             .apply { clone(full_layout) }
             .apply { clearConstraint(this, button1) }
@@ -53,13 +88,13 @@ class ConstraintLayoutChangeAct : AppCompatActivity() {
             .apply { setMargin0(this, button1.id) }
             .apply { setMargin0(this, button2.id) }
             .apply { setMargin0(this, button3.id) }
-            .apply { connect(button1.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, 200) }
+            .apply { connect(button1.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP) }
             .apply { connect(button1.id, ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START) }
             .apply { connect(button1.id, ConstraintSet.END, button2.id, ConstraintSet.START) }
-            .apply { connect(button2.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, 200) }
+            .apply { connect(button2.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP) }
             .apply { connect(button2.id, ConstraintSet.START, button1.id, ConstraintSet.END) }
             .apply { connect(button2.id, ConstraintSet.END, button3.id, ConstraintSet.START) }
-            .apply { connect(button3.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, 200) }
+            .apply { connect(button3.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP) }
             .apply { connect(button3.id, ConstraintSet.START, button2.id, ConstraintSet.END) }
             .apply { connect(button3.id, ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END) }
             .applyTo(full_layout)
@@ -78,7 +113,7 @@ fun clearConstraint(constraintSet: ConstraintSet, view: View, andMargin: Boolean
 fun setMargin0(constraintSet: ConstraintSet, viewId: Int): ConstraintSet {
     return constraintSet
         .apply { setMargin(viewId, ConstraintSet.START, 0) }
-        .apply { setMargin(viewId, ConstraintSet.TOP, 0)  }
-        .apply { setMargin(viewId, ConstraintSet.END, 0)  }
-        .apply { setMargin(viewId, ConstraintSet.BOTTOM, 0)  }
+        .apply { setMargin(viewId, ConstraintSet.TOP, 0) }
+        .apply { setMargin(viewId, ConstraintSet.END, 0) }
+        .apply { setMargin(viewId, ConstraintSet.BOTTOM, 0) }
 }
