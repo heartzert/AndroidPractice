9491ce9105cd3a19b4d8320f23fda548bc6ea0f6
wangxinze <wangxinze@oxyzgroup.com>
2019-04-03 15:38:27 +0800

    test canvas

--- app/src/main/java/heartzert/test/all/customview/views/CanvasTestView.kt
+++ app/src/main/java/heartzert/test/all/customview/views/CanvasTestView.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
+import heartzert.lib.unAbs
 
 /**
  * Created by heartzert on 2019/4/1.
@@ -12,11 +14,35 @@ import android.view.View
  */
 class CanvasTestView(context: Context) : View(context) {
 
+    private lateinit var canvas: Canvas
+    private var mWidth: Int? = null
+    private var mHeight: Int? = null
+
     constructor(context: Context, attributeSet: AttributeSet) : this(context)
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        mWidth = w
+        mHeight = h
+    }
 
     override fun onDraw(canvas: Canvas?) {
         canvas ?: return
+        this.canvas = canvas
         val paint = Paint()
+        val i = 5
+        for (x in 0..100) {
+            Log.d("123123123", unAbs(x).toString())
+        }
+//        testTranslate(paint)
+
+    }
+
+    private fun drawXY(paint: Paint) {
+        canvas.drawLine(mWidth!!.inv().toFloat(), mWidth!!.toFloat(), mHeight!!.inv().toFloat(), mHeight!!.toFloat(),
+            paint)
+    }
+
+    private fun testTranslate(paint: Paint) {
         canvas.translate(100f, 100f)
         canvas.drawCircle(100f, 100f, 50f, paint)
         canvas.translate(100f, 100f)
---
+++ xzlib/src/main/java/heartzert/lib/CommonUtil.kt
@@ -0,0 +1,16 @@
+package heartzert.lib
+
+import kotlin.math.abs
+
+/**
+ * Created by heartzert on 2019/4/2.
+ * Email: heartzert@163.com
+ */
+
+/**
+ * 将参数转为负绝对值
+ */
+fun unAbs(int: Int) = 0 - abs(int)
+fun unAbs(float: Float) = 0f - abs(float)
+fun unAbs(double: Double) = 0.0 - abs(double)
+fun unAbs(long: Long) = 0 - abs(long)
