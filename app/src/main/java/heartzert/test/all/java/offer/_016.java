ee80a47b20ae44294847da1594254fe3a2d941c2
heartzert <heartzert@163.com>
2020-06-30 11:42:40 +0800

    第16、17、18题

---
+++ app/src/main/java/heartzert/test/all/java/offer/_016.java
@@ -0,0 +1,135 @@
+package heartzert.test.all.java.offer;
+
+import java.util.logging.XMLFormatter;
+
+/**
+ * Created by heartzert on 2020/6/28.
+ * Email: heartzert@163.com
+ */
+/*
+实现函数double Power(double base, int exponent)，求base的exponent次方。不得使用库函数，同时不需要考虑大数问题。
+
+ 
+
+示例 1:
+
+输入: 2.00000, 10
+输出: 1024.00000
+示例 2:
+
+输入: 2.10000, 3
+输出: 9.26100
+示例 3:
+
+输入: 2.00000, -2
+输出: 0.25000
+解释: 2-2 = 1/22 = 1/4 = 0.25
+ 
+
+说明:
+
+-100.0 < x < 100.0
+n 是 32 位有符号整数，其数值范围是 [−2^31, 2^31 − 1] 。
+注意：本题与主站 50 题相同：https://leetcode-cn.com/problems/powx-n/
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/shu-zhi-de-zheng-shu-ci-fang-lcof
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+class _016 {
+
+    public static void main(String[] args) {
+        _016 a = new _016();
+        double x = 2.0;
+        int n = 10;
+
+        System.out.println(a.myPow(x, n));
+        System.out.println(a.myPow2(x, n));
+        System.out.println(a.myPow3(x, n));
+        System.out.println(Math.pow(x, n));
+    }
+
+    /*
+    只能想到这种方法，但是看到题目n的范围，这个方法肯定是不对的。
+
+    其实看到n那么大、2^31次方，就应该想到把n转换为二进制来处理了。
+
+    起码前面==0和<0的地方思路对了。（doge）
+     */
+    public double myPow(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        long m = n;
+        if (m < 0) {
+            //这里有取反操作，而题目中范围内，n最小值取反后会超出int最大值，所以将n存到long中处理。
+            //仔细看好题目边界条件！
+            m = -m;
+            x = 1 / x;
+        }
+        double sum = 1;
+        for (int i = 1; i <= m; i++) {
+            sum *= x;
+        }
+        return sum;
+    }
+
+    /*
+    二进制法。
+    将n表示为2进制即x^m=x^(2^0a) * x^(2^1b) * x^(2^2c) * .... * x^(2^31d)
+    其中a...d表示n的二级制表示在该位的值。
+
+    时间复杂度：O(logn)
+     */
+    public double myPow2(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        long m = n;
+        if (m < 0) {
+            m = -m;
+            x = 1 / x;
+        }
+        double sum = 1;
+        while (m != 0) {
+            if ((m & 1) == 1) {
+                //这里如果x是整数，可以用一个标志位i做左移操作。
+                sum *= x;
+            }
+            m = m >>> 1;
+            //由于double不能用左移，题目也不让用库函数，所以只能在每次循环内计算x^2i
+            x *= x;
+        }
+        return sum;
+    }
+
+    /*
+    x^m = (x^2)^(m/2) n为偶数
+    x^m = x(x^2)^(m/2) n为奇数
+
+    啊哈写完代码发现跟上面那个思路写出来一模一样，而我觉得上面那个思路比较好理解
+     */
+    public double myPow3(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        long m = n;
+        if (m < 0) {
+            m = -m;
+            x = 1 / x;
+        }
+        double res = 1;
+        while (m != 0) {
+            //如果是奇数，则需要多乘一个x^(2^i)
+            //10^7 = (10^2)^3 * 10 = (10^4)^1 * 10 * 10^2
+            //此时x = 10^4 = 10^(2^2) ; res = 10 * 10^2
+            if ((m & 1) == 1) {
+                res *= x;
+            }
+            x *= x;
+            m = m >>> 1;
+        }
+        //最后把x与多乘的x相乘即是最后的答案。
+        return res;
+    }
+}
---
+++ app/src/main/java/heartzert/test/all/java/offer/_018.java
@@ -0,0 +1,97 @@
+package heartzert.test.all.java.offer;
+
+/**
+ * Created by heartzert on 2020/6/30.
+ * Email: heartzert@163.com
+ */
+/*
+
+给定单向链表的头指针和一个要删除的节点的值，定义一个函数删除该节点。
+
+返回删除后的链表的头节点。
+
+注意：此题对比原题有改动
+
+示例 1:
+
+输入: head = [4,5,1,9], val = 5
+输出: [4,1,9]
+解释: 给定你链表中值为 5 的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+示例 2:
+
+输入: head = [4,5,1,9], val = 1
+输出: [4,5,9]
+解释: 给定你链表中值为 1 的第三个节点，那么在调用了你的函数之后，该链表应变为 4 -> 5 -> 9.
+
+
+说明：
+
+题目保证链表中节点的值互不相同
+若使用 C 或 C++ 语言，你不需要 free 或 delete 被删除的节点
+ */
+class _018 {
+
+    /**
+     * Definition for singly-linked list.
+     */
+    public static class ListNode {
+
+        int val;
+
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public static void main(String[] args) {
+        _018 a = new _018();
+        ListNode head = new ListNode(4);
+        head.next = new ListNode(5);
+        head.next.next = new ListNode(1);
+        head.next.next.next = new ListNode(9);
+        a.deleteNode(head, 1);
+        ListNode p = head;
+        while (p != null) {
+            System.out.print(p.val + "->");
+            p = p.next;
+        }
+    }
+
+    /*
+    测试用例：
+    1.删除首个
+    2.删除末尾
+    3.删除中间
+    4.只有一个head并删除
+    5.head为null
+    6.删除不存在的值
+
+    注意：题目保证链表中节点的值互不相同，所以查到一个就可以直接返回了。
+     */
+    public ListNode deleteNode(ListNode head, int val) {
+        if (head == null) {
+            return head;
+        }
+        if (head.val == val) {
+            head = head.next;
+            return head;
+        }
+        ListNode p = null;
+        ListNode mid = head;
+        ListNode aft = mid.next;
+        while (true) {
+            if (mid.val == val) {
+                mid.next = null;
+                p.next = aft;
+                break;
+            }
+            if (aft == null) break;
+            p = mid;
+            mid = aft;
+            aft = mid.next;
+        }
+        return head;
+    }
+}
