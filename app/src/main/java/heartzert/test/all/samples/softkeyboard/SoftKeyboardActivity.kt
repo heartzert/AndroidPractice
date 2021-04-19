03ffd99f094e21ed4862a92673cab4ceaf20d6be
WangXinZe <xinze.wang@quvideo.com>
2021-04-19 14:21:41 +0800

    软键盘监听

--- app/src/main/java/heartzert/test/all/samples/softkeyboard/SoftKeyboardActivity.kt
+++ app/src/main/java/heartzert/test/all/samples/softkeyboard/SoftKeyboardActivity.kt
@@ -1,12 +1,12 @@
 package heartzert.test.all.samples.softkeyboard
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import heartzert.lib.utils.tools.SoftKeyboardListener
 import heartzert.test.all.R
 import heartzert.test.all.databinding.ActivitySoftKeyboardBinding
-
 
 class SoftKeyboardActivity : AppCompatActivity() {
 
@@ -15,10 +15,12 @@ class SoftKeyboardActivity : AppCompatActivity() {
     private val softKeyboardListener by lazy {
         SoftKeyboardListener(this, object : SoftKeyboardListener.Callback {
             override fun onKeyboardUp(softKeyboardHeight: Int) {
+                Log.d("==========wxz", "softKeyboardHeight up: $softKeyboardHeight")
                 binding.textView.translationY = -softKeyboardHeight.toFloat()
             }
 
             override fun onKeyboardDown() {
+                Log.d("==========wxz", "softKeyboardHeight down")
                 binding.textView.translationY = 0f
             }
 
--- xzlib/src/main/java/heartzert/lib/utils/tools/SoftKeyboardListener.kt
+++ xzlib/src/main/java/heartzert/lib/utils/tools/SoftKeyboardListener.kt
@@ -1,7 +1,10 @@
 package heartzert.lib.utils.tools
 
 import android.app.Activity
+import android.graphics.Point
 import android.graphics.Rect
+import android.util.Log
+import android.view.View
 import android.view.ViewTreeObserver
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
@@ -12,29 +15,57 @@ import java.lang.ref.SoftReference
  * Created by heartzert on 3/24/21.
  * Email: heartzert@163.com
  */
+//监听键盘弹出收起和键盘高度
 class SoftKeyboardListener(
     activity: Activity,
     private var mCallback: Callback? = null
 ) : LifecycleObserver {
 
+    companion object {
+
+        //键盘打开状态
+        private const val MODE_OPEN = 0
+
+        //键盘关闭状态
+        private const val MODE_CLOSE = 1
+
+        //有些机型相减后是1，所以判断搞大点做兼容
+        private const val minHeight = 10
+    }
+
+    //保存当前的activity
     private var mActivity = SoftReference(activity)
 
-    private var mWindowHeight = 0
+    //绑定的view
+    private var mView: View? = null
 
-    private var mGlobalLayoutListener = ViewTreeObserver.OnGlobalLayoutListener {
-        //获取当前窗口实际的可见区域
-        val r = Rect()
-        activity.window.decorView.getWindowVisibleDisplayFrame(r)
-        val height: Int = r.height()
-        if (mWindowHeight == 0) {
-            //一般情况下，这是原始的窗口高度
-            mWindowHeight = height
-        } else {
-            if (mWindowHeight != height) {
-                //两次窗口高度相减，就是软键盘高度
-                val softKeyboardHeight = mWindowHeight - height
+    private var mGlobalLayoutListener = object : ViewTreeObserver.OnGlobalLayoutListener {
+
+        private var lastMode = MODE_CLOSE
+
+        override fun onGlobalLayout() {
+            mActivity.get() ?: return
+            mView ?: return
+
+            //获取当前窗口实际的可见区域
+            val screenSize = Point()
+            mActivity.get()!!.windowManager.defaultDisplay.getSize(screenSize)
+
+            val r = Rect()
+            mView!!.getWindowVisibleDisplayFrame(r)
+            //两次窗口高度相减，就是软键盘高度
+            val softKeyboardHeight = screenSize.y - r.height()
+
+            //防重入，
+            val thisMode = if (softKeyboardHeight > minHeight) MODE_OPEN else MODE_CLOSE
+            if (thisMode == lastMode) return
+            lastMode = thisMode
+
+            if (softKeyboardHeight > minHeight) {
+                Log.d("====wxz====","softKeyboardHeight up: $softKeyboardHeight")
                 mCallback?.onKeyboardUp(softKeyboardHeight)
             } else {
+                Log.d("====wxz====", "softKeyboardHeight down")
                 mCallback?.onKeyboardDown()
             }
         }
@@ -58,6 +89,24 @@ class SoftKeyboardListener(
         mActivity.get()?.apply {
             window.decorView.viewTreeObserver.removeOnGlobalLayoutListener(mGlobalLayoutListener)
         }
+    }
+
+    /**
+     * 绑定view
+     */
+    fun bind(view: View?) {
+        view ?: return
+        mView = view
+        mView!!.viewTreeObserver.addOnGlobalLayoutListener(mGlobalLayoutListener)
+    }
+
+    /**
+     * 解绑view
+     */
+    fun unBind() {
+        mView ?: return
+        mView!!.viewTreeObserver.removeOnGlobalLayoutListener(mGlobalLayoutListener)
+        mView = null
     }
 
     fun setCallback(callback: Callback) {
